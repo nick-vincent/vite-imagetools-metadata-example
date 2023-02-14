5c1d62c94feb78bb6a53d1fda2d2c81c94a309bd
Nick Vincent <the.nick.vincent@gmail.com>
2023-02-14 12:15:22 -0800

    Add image metadata demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Image from '../img/1600x900.png?fit=inside&webp&w=480&h=480&meta';
+  console.log(Image);
+</script>
+
+<p><code>import Image from '../img/1600x900.png?webp&w=480&h=480&meta';</code></p>
+
+<p><img src={Image.src} /></p>
+
+<ul>
+  <li><code>Image.src: {Image.src} (should be 16:9)</code></li>
+  <li><code>Image.height: {Image.height} (should be 270)</code></li>
+  <li><code>Image.src: {Image.width} (should be 480)</code></li>
+</ul>
